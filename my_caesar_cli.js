40dbee2fd42002c7c4247b500a94b19a3a7ec451
gonzjv <gonzjv@ya.ru>
2021-10-31 14:56:56 +0300

    feat: caesar cipher decoder

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -19,18 +19,18 @@ if (options.pizzaType) {
 }
 if (options.action === 'encode') {
   const caesarEncode = (text, shift) => {
-    const charCodeArr = text.split('').map(
-      (char) =>
+    const charCodeArr = text
+      .split('')
+      .map((char) =>
         char.match(/[A-Z]/)
           ? ((char.charCodeAt(0) - 65 + shift) % 26) + 65
           : char.match(/[a-z]/)
           ? ((char.charCodeAt(0) - 97 + shift) % 26) + 97
           : char.charCodeAt(0),
-      // true ? char.charCodeAt(0) + 1 : char.charCodeAt(0),
-    );
+      );
     const res = String.fromCharCode(...charCodeArr);
     console.log('charCodeArr: ', charCodeArr);
-    console.log('text before: ', text);
+    console.log('before: ', text);
     console.log('result: ', res);
     return res;
   };
@@ -48,8 +48,42 @@ if (options.action === 'encode') {
       error ? console.log(console.error()) : console.log('finished');
     },
   );
-  // console.log('inputFile: ', inputFile);
   console.log('my_caesar_cli: ENCODE done, my master!');
+}
+
+if (options.action === 'decode') {
+  const caesarDecode = (text, shift) => {
+    const charCodeArr = text
+      .split('')
+      .map((char) =>
+        char.match(/[A-Z]/)
+          ? ((char.charCodeAt(0) - 90 - shift) % 26) + 90
+          : char.match(/[a-z]/)
+          ? ((char.charCodeAt(0) - 122 - shift) % 26) + 122
+          : char.charCodeAt(0),
+      );
+    const res = String.fromCharCode(...charCodeArr);
+    console.log('charCodeArr: ', charCodeArr);
+    console.log('before: ', text);
+    console.log('result: ', res);
+    return res;
+  };
+
+  const decodeStream = new Transform({
+    transform(chunk, encoding, callback) {
+      this.push(caesarDecode(chunk.toString(), Number(options.shift)));
+      callback();
+    },
+  });
+
+  pipeline(
+    fs.createReadStream(options.input),
+    decodeStream,
+    fs.createWriteStream(options.output),
+    (error) => {
+      error ? console.log(console.error()) : console.log('finished');
+    },
+  );
 }
 
 console.log('my_caesar_cli in da house!');
