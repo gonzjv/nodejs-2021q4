d4baad8a824e28af5cf7fe89e77b03e62c165474
gonzjv <gonzjv@ya.ru>
2021-11-02 22:31:55 +0300

    refactor: remove commander

--- my_caesar_cli.js
+++ my_caesar_cli.js
@@ -1,23 +1,28 @@
-const commander = require('commander');
 const fs = require('fs');
 const { pipeline, Transform } = require('stream');
+const { stdout, stdin, exit } = process;
 
-const program = new commander.Command();
-
-program
-  .option('-p, --pizza-type <type>', 'flavour of pizza')
-  .option('-s, --shift <number>', 'cipher letter shift')
-  .option('-i, --input <file>', 'input file')
-  .option('-o, --output <file>', 'output file')
-  .option('-a, --action <type>', 'action encode/decode');
-
-program.parse(process.argv);
-
-const options = program.opts();
-if (options.pizzaType) {
-  console.log(`- ${options.pizzaType}`);
+const flags = process.argv.slice(2);
+const requiredFlags = ['-c', '--config'];
+const isRequiredFlags = requiredFlags.some((flag) => flags.includes(flag));
+console.log('isReqFlags: ', isRequiredFlags);
+if (!isRequiredFlags) {
+  console.log('You need to enter valid flags: -c or --config ');
+  exit(1);
 }
-if (options.action === 'encode') {
+
+const getOptionValue = (flag) => {
+  const flagIndex = flags.indexOf(flag);
+  const value = flagIndex !== -1 ? flags[flagIndex + 1] : null;
+  return value;
+};
+
+const configValue = getOptionValue('-c');
+const isCaesarNeeded = configValue && configValue.match(/C/) ? true : false;
+console.log('isCaesar: ', isCaesarNeeded);
+
+if (isCaesarNeeded) {
+  const shiftDefault = 1;
   const caesarEncode = (text, shift) => {
     const charCodeArr = text
       .split('')
@@ -36,8 +41,9 @@ if (options.action === 'encode') {
   };
 
   const getReadStream = () => {
-    if (options.input) {
-      return fs.createReadStream(options.input);
+    const inputValue = getOptionValue('-i');
+    if (inputValue) {
+      return fs.createReadStream(inputValue);
     } else {
       console.log('Text for encode: ');
       return process.stdin;
@@ -46,66 +52,60 @@ if (options.action === 'encode') {
 
   const transformStream = new Transform({
     transform(chunk, encoding, callback) {
-      this.push(caesarEncode(chunk.toString(), Number(options.shift)));
+      this.push(caesarEncode(chunk.toString(), shiftDefault));
       callback();
     },
   });
 
   const getWriteStream = () => {
-    if (options.output) {
-      return fs.createWriteStream(options.output);
+    const outputValue = getOptionValue('-o');
+    if (outputValue) {
+      return fs.createWriteStream(outputValue);
     } else {
       // console.log('Result: ');
       return process.stdout;
     }
   };
 
-  pipeline(
-    // fs.createReadStream(options.input),
-    getReadStream(),
-    transformStream,
-    // fs.createWriteStream(options.output),
-    getWriteStream(),
-    (error) => {
-      error ? console.log(console.error()) : console.log('finished');
-    },
-  );
+  pipeline(getReadStream(), transformStream, getWriteStream(), (error) => {
+    error ? console.log(console.error()) : console.log('finished');
+  });
   // console.log('my_caesar_cli: ENCODE done, my master!');
 }
 
-if (options.action === 'decode') {
-  const caesarDecode = (text, shift) => {
-    const charCodeArr = text
-      .split('')
-      .map((char) =>
-        char.match(/[A-Z]/)
-          ? ((char.charCodeAt(0) - 90 - shift) % 26) + 90
-          : char.match(/[a-z]/)
-          ? ((char.charCodeAt(0) - 122 - shift) % 26) + 122
-          : char.charCodeAt(0),
-      );
-    const res = String.fromCharCode(...charCodeArr);
-    console.log('charCodeArr: ', charCodeArr);
-    console.log('before: ', text);
-    console.log('result: ', res);
-    return res;
-  };
+// if (options.action === 'decode') {
+//   const caesarDecode = (text, shift) => {
+//     const charCodeArr = text
+//       .split('')
+//       .map((char) =>
+//         char.match(/[A-Z]/)
+//           ? ((char.charCodeAt(0) - 90 - shift) % 26) + 90
+//           : char.match(/[a-z]/)
+//           ? ((char.charCodeAt(0) - 122 - shift) % 26) + 122
+//           : char.charCodeAt(0),
+//       );
+//     const res = String.fromCharCode(...charCodeArr);
+//     console.log('charCodeArr: ', charCodeArr);
+//     console.log('before: ', text);
+//     console.log('result: ', res);
+//     return res;
+//   };
 
-  const decodeStream = new Transform({
-    transform(chunk, encoding, callback) {
-      this.push(caesarDecode(chunk.toString(), Number(options.shift)));
-      callback();
-    },
-  });
+//   const decodeStream = new Transform({
+//     transform(chunk, encoding, callback) {
+//       this.push(caesarDecode(chunk.toString(), Number(options.shift)));
+//       callback();
+//     },
+//   });
 
-  pipeline(
-    fs.createReadStream(options.input),
-    decodeStream,
-    fs.createWriteStream(options.output),
-    (error) => {
-      error ? console.log(console.error()) : console.log('finished');
-    },
-  );
-}
+//   pipeline(
+//     fs.createReadStream(options.input),
+//     decodeStream,
+//     fs.createWriteStream(options.output),
+//     (error) => {
+//       error ? console.log(console.error()) : console.log('finished');
+//     },
+//   );
+// }
 
-// console.log('my_caesar_cli in da house!');
+// // console.log('my_caesar_cli in da house!');
