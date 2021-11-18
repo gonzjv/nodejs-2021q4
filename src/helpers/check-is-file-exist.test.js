647b93a0f156a742a55c974d650c4af44a4c280f
gonzjv <gonzjv@ya.ru>
2021-11-18 16:28:24 +0300

    refactor: file exist test

--- src/helpers/check-is-file-exist.test.js
+++ src/helpers/check-is-file-exist.test.js
@@ -1,5 +1,5 @@
 import checkIsFileExist from './check-is-file-exist.js';
-import configError from './config-error.js';
+import fileError from './file-error.js';
 
 const WRONG_PATH = './Tibet/EverestMountain';
 const ERROR_MESSAGE = `Human, stop it!. File ${WRONG_PATH} does not exist`;
@@ -8,6 +8,6 @@ describe('checkIsFileExist', () => {
   test('if user passes -i argument with path that does not exist or with no read access', () => {
     expect(() => {
       checkIsFileExist(WRONG_PATH);
-    }).toThrow(new configError(ERROR_MESSAGE));
+    }).toThrow(new fileError(ERROR_MESSAGE));
   });
 });
