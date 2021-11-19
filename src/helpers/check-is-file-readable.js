40be759c0d39f0995ec3d4d9fd45334deec32d36
gonzjv <gonzjv@ya.ru>
2021-11-19 08:44:05 +0300

    refactor: file readable check

--- src/helpers/check-is-file-readable.js
+++ src/helpers/check-is-file-readable.js
@@ -1,14 +1,15 @@
 import { access, constants } from 'fs';
 import handleFileError from '../errorHandlers/handle-file-error.js';
+import fileError from './file-error.js';
 
 const checkIsFileReadable = (filePath) => {
-  access(filePath, constants.R_OK, (err) => {
-    if (err) {
-      handleFileError(
-        `Wow, wow, wow...easy, friend. File ${filePath} is not readable'}`,
-      );
-    }
-  });
+  try {
+    access(filePath, constants.R_OK);
+  } catch (error) {
+    throw new fileError(
+      `Wow, wow, wow...easy, friend. File ${filePath} is not readable`,
+    );
+  }
 };
 
 export default checkIsFileReadable;
